ed79ee9f722927f13d34b91c55898967c5ee8fd8
kikethor2356 <enrique.alejandro2356amil.com>
2024-01-12 14:54:10 -0600

    Ya esta listo el dar de alta a un administrador

--- LOGYSCYCMX ADMIN/addAdmin.php
+++ LOGYSCYCMX ADMIN/addAdmin.php
@@ -2,14 +2,14 @@
 // Include the database connection file
 require ("CONEXION/conexion.php");
 
-if (isset($_POST['submit'])) {
+if (isset($_POST['submitAdmin'])) {
 	// Escape special characters in string for use in SQL statement	
 	$nomAdm = mysqli_real_escape_string($conex, $_POST['Nombre']);
 	$apellidoAdm = mysqli_real_escape_string($conex, $_POST['Apellido']);
 	$correoAdm = mysqli_real_escape_string($conex, $_POST['Correo']);
 	$passwordAdm = mysqli_real_escape_string($conex, $_POST['Password']);
 
-
+	echo "hola";
 
 	
 	// Check for empty fields
@@ -30,14 +30,13 @@ if (isset($_POST['submit'])) {
 		// Show link to the previous page
 		echo "<br/><a href='javascript:self.history.back();'>Go Back</a>";
 	} else { 
+		echo "hola";
 		// If all the fields are filled (not empty) 
-
-        header ("Location:indexADMIN.html");
 		// Insert data into database
 		$result = mysqli_query($conex, "INSERT INTO  Admin (`Nombre`, `Apellido`,`Correo`,`Password`) VALUES ('$nomAdm', '$apellidoAdm', '$correoAdm','$passwordAdm')");
 		
-		
+		header ("Location:indexADMIN.html");
 	}
-	
+	close($conex);
 }
 ?>
