bb2175571cb4bde0dae2e41bccea2d25be52f3ef
OmarC704 <omaramaralcarbajal1@outlook.com>
2024-01-13 18:17:54 -0600

    cambios
    ya muestra los curriculums enviados al admin

---
+++ LOGYSCYCMX ADMIN/MostrarSolicitudesEmpleo.php
@@ -0,0 +1,53 @@
+<!--
+<link rel="stylesheet" href="Estilos_MisClases.css">
+<script src="Script_MisClases.js"></script>
+-->
+
+
+<?php
+
+$inc = include("CONEXION/conexion.php");
+
+
+
+if ($inc){
+        $consulta = "SELECT * FROM curriculum ";
+        $resultado = mysqli_query($conex,$consulta);
+        if ($resultado){
+
+                while ($row = $resultado->fetch_array()){
+                        $Nombre_Curriculum = $row["Nombres_CURRICULUM"];
+                        $ApeliidoP_Curriculum = $row["ApellidoP_CURRICULUM"];
+                        $ApellidoM_Curriculum = $row["ApellidoM_CURRICULUM"];
+                        $Resumen_Curriculum = $row["Resumen_CURRICULUM"];
+                        $Historial_Curriculum = $row["Historial_CURRICULUM"];
+                        $Formacion_Curriculum = $row["Formacion_CURRICULUM"];
+                        $Correo_Curriculum = $row["Correo_CURRICULUM"];
+                        $Telefono_Curriculum = $row["telefono_CURRICULUM"];
+                        $FechaNacimiento_Curriculum = $row["FechaNacimiento_CURRICULUM"];
+                        $FechaEnvio_Curriculum = $row["FechaEnvio_CURRICULUM"];
+                        $ID_Curriculum = $row["Contador"];
+                        
+                        ?>
+
+                            <tr>
+                                <th> <button><?php echo $Nombre_Curriculum ?></button> </th>
+                            </tr>
+                        
+                        <?php
+
+                }
+
+
+
+
+
+        }
+        
+
+}
+
+
+
+
+?>
