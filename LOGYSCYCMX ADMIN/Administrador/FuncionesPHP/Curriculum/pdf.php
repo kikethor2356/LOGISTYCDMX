0ee8a7b385c049936964a347efda779a291f205b
iMilichu <gmilichu@gmail.com>
2024-02-11 16:06:06 -0600

    Cambios Corregido el diseño del curriculum en pdf, nomas faltan los campos de la imagen del usuario y la vacante, el correo no se puede limitar en espacio, asi que se optó por cambiarlo a color negro por si se pasa del espacio rojo

--- LOGYSCYCMX ADMIN/Administrador/FuncionesPHP/Curriculum/pdf.php
+++ LOGYSCYCMX ADMIN/Administrador/FuncionesPHP/Curriculum/pdf.php
@@ -46,39 +46,38 @@ $html = '
 
     <head>
         <meta charset="utf-8">
-
+        <title>Curriculum: '.$Nombre_Curriculum.'</title>
     </head>
 
     <body>
 
     <div style="background-color: #CD2210; position: absolute; z-index: -2; left: -6%; width: 112%; height: 17.5%; top: 4%"></div>
-    <div style="background-color: #F82812; position: absolute; z-index: -1; left: 5%; width: 20%; height: 109%; top: -4.5%"></div>
+    <div style="background-color: #F82812; position: absolute; z-index: -1; left: 5%; width: 25%; height: 109%; top: -4.5%"></div>
 
-    <title><?php echo "Curriculum:  $Nombre_Curriculum" ?></title>
 
-    <h1 style="position: absolute;color: white;font-family: Calibri;top: 3%; left: 39%;font-size: 250%;">'.$Nombre_Curriculum.'</h1>
+    <h1 style="position: absolute;color: white;font-family: Calibri;top: 3%; left: 32%;font-size: 250%;">'.$Nombre_Curriculum.'</h1>
 
-    <h1 style="position: absolute;color: white;top: 8%;left: 39%;font-family: Calibri;font-size: 250%;">'.$ApeliidoP_Curriculum.'</h1>
+    <h1 style="position: absolute;color: white;top: 8%;left: 32%;font-family: Calibri;font-size: 250%;">'.$ApeliidoP_Curriculum.'</h1>
 
-    <h1 style="position: absolute;color: white;top: 13%;left: 39%;font-family: Calibri;font-size: 250%;"> '.$ApellidoM_Curriculum.'</h1>
-    <h1 style="color: black; position: absolute; left: 41%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 24%">Resumen</h1>
-    <h1 style="position: absolute;color: black;top: 29%;left: 41%;width: 42%;font-family: Calibri;">'.$Resumen_Curriculum.'</h1>
-    <h1 style="color: black; position: absolute; left: 41%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 51%">Historial</h1>
-    <h1 style="position: absolute;color: black;top: 56%;left: 41%;width: 42%;font-family: Calibri;">'.$Historial_Curriculum.'</h1>
-    <div style="position: absolute; left: 19%; border-radius: 50%; overflow: hidden; height: 5%; width: 2.5%; top: 60%; border: 2px solid white"><i class="fa-solid fa-graduation-cap fa-2x" style="color: #ffffff;"></i></div>
-    <h1 style="position: absolute;color: white;top: 62.5%;left: 19.5%;width: 18%;font-family: Calibri;">'.$Formacion_Curriculum.'</h1>
-    <div style="position: absolute; left: 19%; border-radius: 50%; overflow: hidden; height: 5%; width: 2.5%; top: 49%; border: 2px solid white"><i class="fa-solid fa-at fa-2x" style="color: #ffffff; margin-left: 2.5px; margin-top: 2px;"></i></div>
-    <h1 style="position: absolute;color: White;top: 53.5%;left: 19.5%;width: 17%;font-family: Calibri;font-size:110%;">'.$Correo_Curriculum.'</h1>
-    <div style="position: absolute; left: 19%; height: 4.8%; width: 2.3%; border: 2px solid white; top: 36%; border-radius: 50%; overflow: hidden"><i class="fa-solid fa-phone-volume fa-2x" style="color: #ffffff;"></i></div>
-    <h1 style="position: absolute;color: White;top: 38.5%;left: 19.5%;font-family: Calibri;">'.$Telefono_Curriculum.'</h1>
-    <div style="position: absolute; left: 19%; border-radius: 50%; overflow: hidden; height: 5%; width: 2.5%; top: 81%; border: 2px solid white"><i class="fa-solid fa-cake-candles fa-2x" style="color: #ffffff; margin-left: 4.2px;"></i></div>
-    <h1 style="position: absolute;color: White;top: 78%;left: 22.3%;font-family: Calibri;">'.$FechaNacimiento_Curriculum.'</h1>
+    <h1 style="position: absolute;color: white;top: 13%;left: 32%;font-family: Calibri;font-size: 250%;"> '.$ApellidoM_Curriculum.'</h1>
+    <h1 style="color: black; position: absolute; left: 35%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 21%">Resumen</h1>
+    <h1 style="position: absolute;color: black;top: 25%;left: 35%;width: 68%;font-family: Calibri;">'.$Resumen_Curriculum.'</h1>
+    <h1 style="color: black; position: absolute; left: 35%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 50%">Historial</h1>
+    <h1 style="position: absolute;color: black;top: 55%;left: 35%;width: 42%;font-family: Calibri;">'.$Historial_Curriculum.'</h1>
+    <div style="position: absolute; left: 7%; height: 2%; width: 5%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 39%; font-size: 140%; color: white">Formación</div>
+    <h1 style="position: absolute;color: white;top: 41%;left: 7%;width: 21%;font-family: Calibri; font-size: 140%;">'.$Formacion_Curriculum.'</h1>
+    <div style="position: absolute; left: 7%; height: 2%; width: 5%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 32%; font-size: 140%; color: white">Correo</div>
+    <h1 style="position: absolute;color: black;top: 34%;left: 7%;width: 17%;font-family: Calibri;font-size:80%;">'.$Correo_Curriculum.'</h1>
+    <div style="position: absolute; left: 7%; height: 2%; width: 5%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 23%; font-size: 140%; color: white">Teléfono</div>
+    <h1 style="position: absolute;color: White;top: 25%;left: 7%;font-family: Calibri; font-size: 140%">'.$Telefono_Curriculum.'</h1>
+    <div style="position: absolute; left: 7%; height: 2%; width: 21%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 58%; font-size: 120%; color: white">Fecha de nacimiento</div>
+    <h1 style="position: absolute;color: White;top: 61%;left: 7%;font-family: Calibri; font-size: 150%">'.$FechaNacimiento_Curriculum.'</h1>
 
-    <h1 style="position: absolute;color: white;top: 90%;left: 19%;font-family: Calibri;">'.$FechaEnvio_Curriculum.'</h1>
+    <h1 style="position: absolute;color: white;top: 95%;left: 7%;font-family: Calibri;">'.$FechaEnvio_Curriculum.'</h1>
 
-    <h1 style="position: absolute;color: black;top: 90%;left: 80%;font-family: Verdana, Geneva, Tahoma, sans-serif;">'.$ID_Curriculum.'</h1>
+    <h1 style="position: absolute;color: black;top: 95%;left: 95%;font-family: Verdana, Geneva, Tahoma, sans-serif;">'.$ID_Curriculum.'</h1>
 
-    <h1 style="color: black; position: absolute; left: 41%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 85%">Vacante</h1>
+    <h1 style="color: black; position: absolute; left: 35%; font-family: Verdana, Geneva, Tahoma, sans-serif; top: 85%">Vacante</h1>
 
         </body>
 
