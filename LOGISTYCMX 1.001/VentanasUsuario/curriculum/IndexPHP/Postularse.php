5ea8df7586bfbe630af36951d6218d58198ac3f8
TheGileS46 <edgar.ramos3005@hotmail.com>
2024-02-13 03:21:53 -0600

    formulario CV

--- LOGISTYCMX 1.001/VentanasUsuario/curriculum/IndexPHP/Postularse.php
+++ LOGISTYCMX 1.001/VentanasUsuario/curriculum/IndexPHP/Postularse.php
@@ -10,91 +10,143 @@
 
 
 
-<body>
+    <body>
 
-<section id="header">
+        <section id="header">
             <a href="#"><img src="" class="logo" alt="Logo"></a>
     
             <div id="Navbar">
                 <li><a href="/LOGISTYCMX 1.001/VentanasUsuario/curriculum/IndexPHP/Postularse.php">Postularse</a></li>
-                <li><a href="/LOGISTYCMX 1.001/VentanasUsuario/Landing/landing_index.html">Contactanos</a></li>
-                <li><a href="#Nosotros">Nosotros</a></li>    
+                <li><a href="/LOGISTYCMX 1.001/VentanasUsuario/Landing/landing_index.php">Contactanos</a></li>
+                <li><a href="/LOGISTYCMX 1.001/VentanasUsuario/Landing/landing_index.php">Nosotros</a></li>    
             </div>
         </section>
 
-  
-        <table>
-            <thead>
-                <tr>
-                    <th>N</th>
-                    <th>Vacante</th>
-                    <th>Descripcion</th>
-                </tr>
-            </thead>
-
-            <tbody>
-            <?php
-            include("..\FuncionesPHP\GeneradorVacantes.php");
-            ?>
-            </tbody>
-        </table>
-        
-        <div id="Form">
-            <div class="txtform">
-                <h1>Envia Tu curriculum</h1>
+        <div class="container-boss">
+            <div class="container">
+                <img src="/LOGISTYCMX 1.001/VentanasUsuario/Landing/assets/refacciones-auto.jpg" alt="">
+                <form  id="mainForm" method="post" action="\LOGISTYCMX 1.001\VentanasUsuario\curriculum\FuncionesPHP\GUARDAR_curriculum.php" enctype="multipart/form-data">
+                    <h2>Postulate</h2>
+                    <div class="form-control">
+                        <label for="name">Nombre(s)</label>
+                            <input 
+                                type="text" 
+                                placeholder="Ingresa tu Nombre(s)" 
+                                name="field_NombresC" 
+                            required>
+                    </div>
+    
+                    <div class="form-control">
+                        <label>Apellido Paterno</label>
+                        <input 
+                            type="text" 
+                            placeholder="Ingresa tu Apellido Paterno" 
+                            name="field_ApellidoPC" 
+                            required 
+                        >
+                    </div>
+    
+                    <div class="form-control">
+                        <label>Apellido Materno</label>
+                        <input 
+                            type="text" 
+                            placeholder="Ingresa tu Apellido Materno" 
+                            name="field_ApellidoMC" 
+                            required 
+                        >
+                    </div>
+    
+    
+                    <div class="form-control">
+                        <label>Resumen Profesional</label>
+                        <input 
+                            type="text" 
+                            placeholder="Resumen Profesional" 
+                            name="field_ResumenC" 
+                            required 
+                        >
+                    </div>
+    
+    
+                    <div class="form-control">
+                        <label>Resumen Laboral</label>
+                        <input 
+                            type="text" 
+                            placeholder="Historial Laboral" 
+                            name="field_HistorialC" 
+                            required 
+                        >   
+                    </div>
+    
+                    <div class="form-control">
+                        <label>Formacion Academica</label>
+                        <input 
+                            type="text" 
+                            placeholder="Formacion Academica" 
+                            name="field_FormacionC" 
+                            required 
+                        >
+                    </div>
+    
+                    <div class="form-control">
+                        <label>Vacantes Disponibles</label>
+                        <select name="ComboVacante">
+                            <?php
+                            include("..\FuncionesPHP\GeneradorComboVacante.php");
+                             ?>
+                        </select>
+                    </div>
+    
+                    <div class="form-control">
+                        <label for="email">Correo Electronico</label>
+                        <input 
+                            type="text" 
+                            placeholder="Escribe tu Correo Electronico" 
+                            name="field_CorreoC" 
+                            required 
+                        >
+                    </div>
+                    
+                    <div class="form-control">
+                        <label for="number">Numero Celular</label>
+                        <input 
+                            type="text" 
+                            placeholder="Ingresa tu numero celular" 
+                            name="field_TelefonoC" 
+                            required 
+                        >
+                    </div>
+    
+                    <div class="form-control">
+                        <label>Fecha de Nacimiento</label>
+                        <input 
+                            type="text" 
+                            placeholder="DD/MM/AAAA" 
+                            name="field_FechaNacimientoC" 
+                            required 
+                        >
+                    </div>
+    
+                    <div class="form-control">
+                        <label>Ingresa una foto tuya</label>
+                        <input 
+                            type="file" 
+                            name="fieldfoto" 
+                            id="foto"
+                        >
+                    </div>  
+                    <button type="submit" name="btnEnviarCurriculum">Enviar curriculum</button>   
+                </form>
+                
             </div>
-
-            <div class="Cv-form">
-                <form method="post" action="\LOGISTYCMX 1.001\VentanasUsuario\curriculum\FuncionesPHP\GUARDAR_curriculum.php" enctype="multipart/form-data">
-                    <label for="name">Nombre(s)</label>
-                    <input type="text" placeholder="Nombre(s)" name="field_NombresC" required >
-
-                    <label for="name">Apellido Paterno</label>
-                    <input type="text" placeholder="Apellido Paterno" name="field_ApellidoPC" required >
-            
-
-                    <label for="name">Apellido Materno</label>
-                    <input type="text" placeholder="Apellido Materno" name="field_ApellidoMC" required >
-            
-
-            
-                    <input type="text" placeholder="Resumen Profesional" name="field_ResumenC" required >
-            
-
-            
-                    <input type="text" placeholder="Historial Laboral" name="field_HistorialC" required >
-            
-           
-                    <input type="text" placeholder="Formacion Academica" name="field_FormacionC" required >
-            
-            
-                    <select name="ComboVacante">
-                        <?php
-                                    include("..\FuncionesPHP\GeneradorComboVacante.php");
-                        ?>
-                    </select>
-            
-          
-                    <input type="text" placeholder="Correo Electronico" name="field_CorreoC" required >
-            
-            
-                    <input type="text" placeholder="Telefono" name="field_TelefonoC" required >
-                        
-                    <input type="text" placeholder="DD/MM/AAAA" name="field_FechaNacimientoC" required >
-            
-                    <input type="file" name="fieldfoto" id="foto">
-            
-                    <button type="submit" name="btnEnviarCurriculum">Enviar curriculum</button>
-                    </form> 
-            </div>
-
         </div>
-        
 
         
 
-  
 
-</body>
+         
+        
+
+    </body>
 
 </html>
